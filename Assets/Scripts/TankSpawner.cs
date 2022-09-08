b029d1df022dffc33ad4dc1532f940adbe5c0356
AbhiDashadiya <109543748+AbhiDashadiya@users.noreply.github.com>
2022-09-08 09:50:24 +0530

    TankSpawnScript
    Tank spawn script and tank prefab

---
+++ Assets/Scripts/TankSpawner.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankSpawner : MonoBehaviour
+{
+    [SerializeField] public GameObject tankPrefb;
+    
+
+    void Start()
+    {
+        Instantiate(tankPrefb,transform.position,Quaternion.identity);
+    }
+
+    
+}
